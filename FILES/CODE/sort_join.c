17c48b82c0f580a2bae628c909aa732b636d42ef
TheoKant <kantartzistheophilos@gmail.com>
2019-11-06 19:41:13 +0200

    hist and psum

--- FILES/CODE/sort_join.c
+++ FILES/CODE/sort_join.c
@@ -3,54 +3,100 @@
 #include <stdlib.h>
 #include "../HEADERS/sort_join.h"
 
+
+// Function rec_sort used to sort the array
+
 void rec_sort ( relation *rel , int start , int end , int bnum ) {
 	int size = calculate_size( start , end ) ;
 
 	if ( size <= 64000 ) {
 		//quicksort
-		printf ( "THIS IS WHERE WE QUICKSORT\n" ) ;
+	
 	}
 	else {
-		//CREATE OUR BUCKET
-		tuple *hist = malloc ( 256 * sizeof ( tuple ) ) ; //Hist is size of 2^8 = 256
-		hist = create_hist ( rel , start , end , bnum ) ;
+		//Create hist for the part of the relation we are checking
+		int *hist = malloc ( 256 * sizeof ( int ) ) ; //Hist is size of 2^8 = 256
+		hist = create_hist ( rel , hist , start , end , bnum ) ; //scan the relation and fill the hist
+		//Check which cells of hist arent 0 so we can determine the size of psum then fill it according to the given hist
+		int psum_size = get_psumsize( hist );
+		int **psum = malloc ( psum_size * sizeof ( int ) ) ;
+		for (int i = 0 ; i < psum_size ; i++ ){
+			psum[i] = malloc (2 * sizeof ( int ) ) ;
+		}
+		fill_psum ( psum , hist , psum_size );
+
+		for ( int i = 0 ; i < psum_size ; i++ ){
+			printf("BYTE IS %d AND IT STARTS FROM %d\n", psum[i][0],psum[i][1] );
+		}
 	}
 
 }
 
+//Fill psum according to given hist
 
+void fill_psum ( int **psum , int *hist , int psum_size ) {
+	int i = 0; //idenitifier for psum array
+	int carry = 0; //carries the hist value of the last checked so we can add it to the next
 
+	for ( int h = 0 ; h < 255 ; h++ ) {
+		if ( hist[h] != 0 ) {
+			if ( i == 0 ) { //first cell of psum starts from zero
+				psum[i][0] = h;
+				psum[i][1] = 0; 
+				carry = hist[h] ;
+				i++;
+			}
+			else { //ist cell of psum starts from last cell + last cells carry
+				psum[i][0] = h;
+				psum[i][1] = psum[i-1][1] + carry;
+				carry = hist[h] ;
+				i++;
+			}
+		}
+	}
+}
 
+//Returns requiered size for the psum
 
+int get_psumsize ( int *hist ) {
+	int psum_size = 0;
+	for (int i = 0 ; i <= 255 ; i++ ) {
+		if ( hist[i] != 0 ) {
+			psum_size++;
+		}
+	}
+	return psum_size;
+}
 
+//Create Hist
 
-
-
-
-
-
-
-tuple *create_hist ( relation *rel , int start , int end , int bnum ) {
-	int total_tuples = end - start + 1 ;
+int *create_hist ( relation *rel , int *hist , int start , int end , int bnum ) {
 
 	uint64_t s_byte;
 	uint64_t key;
 
-	for (int i = start ; i <= end ; i++ ) {
-		
-		key = relation_getkey ( rel , i ) ;
-		s_byte = key << ( bnum - 1 ) * 8 ;
-		s_byte = s_byte >> 56 ;
-		printf("KEY IS %ld\n" , s_byte ) ;
-
+	//Set all of hist cells to 0
+	for (int h = start ; h <= 255 ; h++ ) {
+		hist[h] = 0;
 	}
+	//Check all of the relation's tuples' bnum significant byte and add 1 to the matching hist cell
+	for (int i = start ; i <= end ; i++ ) {
+		key = relation_getkey ( rel , i ) ;
+		s_byte = get_sigbyte ( key , bnum ) ;
+		hist[s_byte]++;
+	}
+	return hist; 
 }
 
+//Returns i significant byte of the key
 
+uint64_t get_sigbyte ( uint64_t key , int i ) {
+	uint64_t s_byte = key << ( i - 1 ) * 8 ;
+	s_byte = s_byte >> 56 ;
+	return s_byte;
+}
 
-
-
-
+//Calculate size (bytes) of sub-relation
 
 int calculate_size ( int start , int end ) {
 	/*  EXAMPLE : WHOLE RELATION <-- 60 tuples
--- FILES/HEADERS/sort_join.h
+++ FILES/HEADERS/sort_join.h
@@ -6,14 +6,26 @@
 
 void rec_sort ( relation * /*relation*/ , int /*start*/ , int /*end*/ , int /*significant byte no*/ ) ;
 
-//Calculate size of sub-relation
+//Calculate size (bytes) of sub-relation
 
 int calculate_size ( int /*start*/ , int /*end*/ ) ;
 
 //Create Hist
 
-tuple *create_hist ( relation * , int , int , int ) ;
+int *create_hist ( relation * , int * , int , int , int ) ;
 
 //Funtction print_array
 
 void print_array ( int ** /*array*/ , int /*size of array*/ ) ;
+
+//Returns i significant byte of the key
+
+uint64_t get_sigbyte ( uint64_t , int ) ;
+
+//Returns requiered size for the psum
+
+int get_psumsize ( int * ) ;
+
+//Fill psum according to given hist
+
+void fill_psum ( int ** , int * , int ) ;
